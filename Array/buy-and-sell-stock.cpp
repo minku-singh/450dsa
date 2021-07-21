eb9e54325d7d13de13b3ab3c3a15561f3eb276ca
Minku Singh <mailminkusingh@gmail.com>
2021-07-21 13:16:22 +0530

    DP

---
+++ Array/buy-and-sell-stock.cpp
@@ -0,0 +1,48 @@
+/*
+Best Time to Buy and Sell Stock
+
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int bestTime(int prices[], int n)
+{
+    int lsf = prices[0];
+    int pist = 0;
+    int op = 0;
+
+    for(int i = 0; i < n; i++)
+    {
+        if(prices[i] < lsf)
+        {
+            lsf = prices[i];
+        }
+
+        pist = prices[i] - lsf;
+
+        if(pist > op)
+        {
+            op = pist;
+        }
+    }
+    return op;
+}
+
+int main()
+{
+    int prices[] = {7,1,5,3,6,4};
+    int n = sizeof(prices)/sizeof(prices[0]);
+
+    cout << bestTime(prices, n);
+}
