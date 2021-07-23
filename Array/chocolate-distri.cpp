2091272514744dbbd4436758f2153501a18e7c2f
Minku Singh <mailminkusingh@gmail.com>
2021-07-23 23:24:11 +0530

    windowing

---
+++ Array/chocolate-distri.cpp
@@ -0,0 +1,51 @@
+/*
+Chocolate Distribution Problem 
+
+Given an array A[ ] of positive integers of size N, where each value represents the number of chocolates in a packet. Each packet can have a variable number of chocolates. There are M students, the task is to distribute chocolate packets among M students such that :
+1. Each student gets exactly one packet.
+2. The difference between maximum number of chocolates given to a student and minimum number of chocolates given to a student is minimum.
+
+Input:
+N = 8, M = 5
+A = {3, 4, 1, 9, 56, 7, 9, 12}
+Output: 6
+Explanation: The minimum difference between 
+maximum chocolates and minimum chocolates 
+is 9 - 3 = 6 by choosing following M packets :
+{3, 4, 9, 7, 9}.
+*/
+
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+int minGap(int Arr[], int n, int m)
+{
+    int ans = INT_MAX;
+    sort(Arr, Arr+n);
+    for(int i = 0; i <= n-m; i++)
+    {
+        int minw = Arr[i];
+        int maxw = Arr[i+m-1];
+
+        int gap = maxw - minw;
+
+        if(gap < ans)
+        {
+            ans = gap;
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    int Arr[]= {3, 4, 1, 9, 56, 7, 9, 12};
+    int n = sizeof(Arr)/sizeof(Arr[0]);
+    int m = 5;
+
+    cout << minGap(Arr, n, m);
+
+    return 0;
+}
